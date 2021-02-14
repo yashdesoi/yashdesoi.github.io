8ff32973f36a3f0d1476ec9bedd4fbc6e0b68997
YasH <yashdesoi@gmail.com>
2021-02-14 21:04:59 +0530

    made navbar responsive

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ introduction.style.filter = 'blur(0px)';
 
 
 // Fading the background color of nav-bar and toggling arrow-up button upon scrolling certain height
-const navBar = document.querySelector('.main-nav');
+const header = document.querySelector('header');
 const overviewSection = document.getElementById('overview');
 const arrowUp = document.querySelector('.arrow-up');
 
 window.addEventListener('scroll', () => {
 
     if (window.pageYOffset > 260) {
-        navBar.style.backgroundColor = 'black';
+        header.style.backgroundColor = 'black';
     } else {
-        navBar.style.backgroundColor = 'transparent';
+        header.style.backgroundColor = 'transparent';
     }
 
     if (window.pageYOffset >= overviewSection.offsetHeight) {
@@ -25,6 +25,7 @@ window.addEventListener('scroll', () => {
 });
 
 // Adding functionality to nav-bar
+const navBar = document.querySelector('.main-nav');
 const navItems = document.querySelector('.main-nav__items');
 
 navItems.addEventListener('click', event => {
@@ -35,11 +36,12 @@ navItems.addEventListener('click', event => {
         window.scrollTo({
             top: section.offsetTop - navBar.offsetHeight,
             left: 0,
-            behaviour: 'smooth'
+            behavior: 'smooth'
         });
     }
 });
 
+// Directly jump to about section
 const arrowDown = document.querySelector('.arrow-down');
 const aboutSection = document.getElementById('about');
 
@@ -51,10 +53,43 @@ arrowDown.addEventListener('click', () => {
     });
 });
 
+// Jump back to top of the document
 arrowUp.addEventListener('click', () => {
-    window.scroll({
+    window.scrollTo({
         top: 0,
         left: 0,
-        behaviour: 'smooth'
+        behavior: 'smooth'
     })
 });
+
+// Making nav-bar responsive
+const dropdown = document.querySelector('.main-nav__items');
+let dropdownTrueHeight = 180; // Hard coded
+
+if (window.innerWidth <= 830) {
+    dropdownTrueHeight = dropdown.offsetHeight;
+    dropdown.style.height = '0px';
+}
+
+const menuButton = document.querySelector('.menu-icon');
+let dropdownClicks = 0;
+
+menuButton.addEventListener('click', () => {
+    dropdownClicks += 1;
+    if (dropdownClicks%2 === 1) {
+        dropdown.style.height = `${dropdownTrueHeight}px`;
+    } else {
+        dropdown.style.height = `0px`;
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 830) {
+        dropdown.style.height = '0px';
+    } else {
+        dropdown.style.height = 'auto';
+    }
+});
+
+
+
