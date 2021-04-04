8c06c233591e500bc739cb90146bab207c8e2f33
YasH <yashdesoi@gmail.com>
2021-04-04 17:28:51 +0530

    minor changes

--- service-worker.js
+++ service-worker.js
@@ -4,3 +4,17 @@ workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
 );
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(async function() {
+        try{
+            var res = await fetch(event.request);
+            var cache = await caches.open('cache');
+            cache.put(event.request.url, res.clone());
+            return res;
+        }
+        catch(error){
+            return caches.match(event.request);
+        }
+    }());
+});
